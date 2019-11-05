ab75265424cdf903811b87727d6fe8cad033570e
Christi Thompson <christithomp@tamu.edu>
2019-11-05 13:59:50 -0600

    Add comments to get_logit function

--- R/get_logits.R
+++ R/get_logits.R
@@ -8,11 +8,14 @@
 #'
 #' @examples
 get_logits = function(new_data){
-  p = ncol(new_data)
-  n = nrow(new_data)
+  p = ncol(new_data) # Get number of columns of data
+  n = nrow(new_data) # Get number of rows of data
 
+  # Make predictions for the logit
   fHat = predict(fit, newdata = new_data, type = "link")
+  # Calculate range of y values
   yrange = c(min(fHat), max(fHat)
 
+  # Return list containing fHat and yrange
   return(list(fHat = fHat, yrange = yrange))
 }
