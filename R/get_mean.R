1c700739a954101ab7d190b097bc563008f3b871
Christi Thompson <christithomp@tamu.edu>
2019-11-05 13:32:26 -0600

    Add comments to function

--- R/get_mean.R
+++ R/get_mean.R
@@ -9,11 +9,13 @@
 #'
 #' @examples
 get_mean = function(data, ng){
-  p = ncol(data)
+  p = ncol(data) # Get number of variables in data
 
-  var_means = matrix(0, ng, p)
+  var_means = matrix(0, ng, p) # Create empty matrix to store means
   for (i in 1:p){
+    # Find mean of each variable and repeat that ng times
     var_means[ , i] = rep(mean(data[ , i]), ng)
   }
+  # Return matrix of means
   return(var_means)
 }
