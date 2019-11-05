1563d7dece02181867d391ed68ce5117c3f5ade8
Christi Thompson <christithomp@tamu.edu>
2019-11-05 14:15:09 -0600

    Add Roxygen skeleton

--- R/get_plot.R
+++ R/get_plot.R
@@ -1,4 +1,18 @@
 
+#' Title
+#'
+#' @param x - dataframe or matrix containing spline terms
+#' @param fHat - matrix of prediction values
+#' @param yrange - matrix containing minimum and maximum for y axis
+#' @param title - string for title of the plot
+#' @param x_lab - string for title of x axis
+#' @param y_lab - string for title of y axis
+#' @param color - color of the line on the plot
+#'
+#' @return
+#' @export
+#'
+#' @examples
 get_plot = function(x, fHat, yrange, title, x_lab, y_lab, color){
   p = ncol(data) # Get number of columns of data
 
