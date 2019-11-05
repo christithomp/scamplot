8b75224cc9d69c0a94dda4eaded7b4a420f9dba6
Christi Thompson <christithomp@tamu.edu>
2019-11-05 13:52:16 -0600

    Add function to calculate logits from prediction and yrange

---
+++ R/get_logits.R
@@ -0,0 +1,16 @@
+
+get_logits = function(new_data){
+  p = ncol(new_data)
+  n = nrow(X)
+
+  fHat = matrix(0, n, p)
+  yrange = matrix(0, p, 2)
+
+  for(i in 1:p){
+    fHat[ , i] = predict(fit, newdata = new_data[ , i], type = "link")
+    yrange[i, 1] = min(fHat[ , i])
+    yrange[i, 2] = max(fHat[ , i])
+  }
+
+  return(list(fHat = fHat, yrange = yrange))
+}
