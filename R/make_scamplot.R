4333b7ee3e2a57ac928f653eeac1a2545328da1c
Christi Thompson <christithomp@tamu.edu>
2019-11-13 11:11:56 -0600

    Add comments for what checks need to be made

--- R/make_scamplot.R
+++ R/make_scamplot.R
@@ -1,4 +1,16 @@
 
-make_scamplot = function(data, y, smooth_terms, linear_terms, type, title = "Prediction Plot", color = "black"){
+make_scamplot = function(data, y, smooth_terms, linear_terms, type, title = "Prediction Plot"){
+  # Check if y is just one vector
+
+  # Check if y is a column in data
+
+  # Check if smooth_terms are coumns in data
+
+  # Check if linear_terms are coumns in data
+
+  # Check if type is of the right form
+
+  # Check if title is string
+
 
 }
