ab02d3f82d085a3f35945ba3e07527eb16a5844d
Christi Thompson <christithomp@tamu.edu>
2019-11-13 11:19:06 -0600

    Add check for title being a string

--- R/make_scamplot.R
+++ R/make_scamplot.R
@@ -2,17 +2,19 @@
 make_scamplot = function(data, y, smooth_terms, linear_terms, type, title = "Prediction Plot"){
   # Check if y is just one vector
   if (!is.vector(y)){
-    paste("Error: Argument y supplied is not a vector")
+    paste("Error: y argument supplied is not a vector")
   }
   # Check if y is a column in data
 
-  # Check if smooth_terms are coumns in data
+  # Check if smooth_terms are columns in data
 
-  # Check if linear_terms are coumns in data
+  # Check if linear_terms are columns in data
 
   # Check if type is of the right form
 
   # Check if title is string
-
+  if(!is.string(title)){
+    paste("Error: title argument supplied is not a strong")
+  }
 
 }
