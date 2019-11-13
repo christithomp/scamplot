9c1962b636856ecdaf772ceb54cc118cc3299554
Christi Thompson <christithomp@tamu.edu>
2019-11-13 10:54:20 -0600

    Change ncol to length, which changed names(term)[i] to just term[i]

--- R/get_model.R
+++ R/get_model.R
@@ -10,24 +10,24 @@
 #'
 #' @examples
 get_model = function(y, smooth_terms, linear_terms){
-  p_smooth = ncol(smooth_terms) # get numer of spline terms
-  p_linear = ncol(linear_terms) # get number of linear terms
+  p_smooth = length(smooth_terms) # get numer of spline terms
+  p_linear = length(linear_terms) # get number of linear terms
 
   # Begin building call to model
-  var = paste("s(", names(smooth_terms)[1], ")", sep = "")
-  scam_model = "y ~ var"
+  var = paste("s(", smooth_terms[1], ")", sep = "")
+  scam_model = paste("y ~ ", var, sep = "")
 
   # Loop over adding each spline term to model
   if (p_smooth > 1){
     for (i in 2:p_smooth){
-      var = paste("s(", names(smooth_terms)[i], ")", sep = "")
+      var = paste("s(", smooth_terms[i], ")", sep = "")
       scam_model = paste(scam_model, var, sep = " + ")
     }
   }
 
   # Loop over adding each linear term to model
-  for (i in 2:p_smooth){
-    var = names(linear_terms)[i]
+  for (i in 1:p_linear){
+    var = linear_terms[i]
     scam_model = paste(scam_model, var, sep = " + ")
   }
   # Return string of call to model
