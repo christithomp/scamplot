2470b8ff358aed438f0218985ceb8998e30497f4
Christi Thompson <christithomp@tamu.edu>
2019-11-13 11:11:22 -0600

    Get rid of color arguemnt in get_plot

--- R/get_plot.R
+++ R/get_plot.R
@@ -13,12 +13,12 @@
 #' @export
 #'
 #' @examples
-get_plot = function(x, fHat, yrange, title, x_lab, y_lab, color){
+get_plot = function(x, fHat, yrange, title, x_lab, y_lab){
   p = ncol(data) # Get number of columns of data
 
   # Plot each variable
   for(i in 1:p){
-    plot(x[ ,i], fHat[ ,i], col =  color, xlab = x_lab[i],
+    plot(x[ ,i], fHat[ ,i], xlab = x_lab[i],
          ylab = y_lab, ylim = c(yrange[i, 1], yrange[i, 2]),
          type = "l", lwd = 2, main = title)
   }
