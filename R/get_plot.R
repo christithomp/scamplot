914a003926cfa4a283131ea72f50faad346295fc
Christi Thompson <christithomp@tamu.edu>
2019-11-18 15:06:30 -0600

    Change plot to not include loop (which changes all matrices to vectors instead)

--- R/get_plot.R
+++ R/get_plot.R
@@ -1,14 +1,13 @@
 
 #' Predictive Plot for Model
 #'
-#' @param x - dataframe or matrix containing spline terms
-#' @param fHat - matrix of prediction values
-#' @param lb- mvector containing  lower bound of confidence interval
+#' @param x - vector containing spline terms
+#' @param fHat - vector of prediction values
+#' @param lb- vector containing  lower bound of confidence interval
 #' @param ub -vector containing upper bound of confidence interval
 #' @param title - string for title of the plot
 #' @param x_lab - string for title of x axis
 #' @param y_lab - string for title of y axis
-#' @param color - color of the line on the plot
 #'
 #' @return
 #' @export
@@ -16,11 +15,11 @@
 #' @examples
 get_plot = function(x, fHat, lb, ub, title, x_lab, y_lab){
   p = ncol(data) # Get number of columns of data
+  #Function to calculate expit of x
+  expit <- function(x) return(1/(1 + exp(-x)))
 
-  # Plot each variable
-  for(i in 1:p){
-    plot(x[ ,i], fHat[ ,i], xlab = x_lab[i],
-         ylab = y_lab, ylim = c(yrange[i, 1], yrange[i, 2]),
-         type = "l", lwd = 2, main = title)
-  }
+  plot(0, type = 'n', xlab = x_lab[i], ylab = y_lab, xlim = c(0, 1), ylim = c(0, 1),
+       type = "l", lwd = 2, main = title)
+  polygon(c(x, rev(x)), c(expit(lb)), rev(expit(ub)), col = 'palegreen', border = FALSE)
+  lines(x, expit(fHat), col = "darkgreen", lwd = 2)
 }
