298b57dce48184781d42a2f29a64114ee4a6f3b7
Christi Thompson <christithomp@tamu.edu>
2019-11-19 11:27:55 -0600

    Add comments into function for plotting

--- R/get_plot.R
+++ R/get_plot.R
@@ -14,18 +14,27 @@
 #'
 #' @examples
 get_plot = function(x, fHat, lb, ub, title, x_lab, y_lab, type){
-  p = ncol(data) # Get number of columns of data
+  # Get number of columns of data
+  p = ncol(data)
+
   #Function to calculate expit of x
   expit <- function(x) return(1/(1 + exp(-x)))
 
+  # Creat basic plot template
   plot(0, type = 'n', xlab = x_lab[i], ylab = y_lab, xlim = c(0, 1), ylim = c(0, 1),
        type = "l", lwd = 2, main = title)
+  # Check if plotting type = link
   if (type == "link"){
+    # Create shaded confidence region
     polygon(c(x, rev(x)), c(lb), rev(ub), col = 'palegreen', border = FALSE)
+    # Create line of function shape
     lines(x, fHat, col = "darkgreen", lwd = 2)
   }
+  # Check if plotting type = response
   else{
+    # Create shaded confidence region
     polygon(c(x, rev(x)), c(expit(lb)), rev(expit(ub)), col = 'palegreen', border = FALSE)
+    # Create line of function shape
     lines(x, expit(fHat), col = "darkgreen", lwd = 2)
   }
 }
