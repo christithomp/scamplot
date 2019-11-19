f54aac96e8cfde254ab0b51d81c2f5e0ce6deffa
Christi Thompson <christithomp@tamu.edu>
2019-11-19 11:32:57 -0600

    Add additional comments to function

--- R/get_model.R
+++ R/get_model.R
@@ -24,6 +24,7 @@ get_model = function(y, smooth_terms, linear_terms, shape_type){
 
   # Begin building call to model with first term
   var = paste("s(", smooth_terms[1], ", bs = '", shape_type[1], "')", sep = "")
+  # Add new term to model
   scam_model = paste(y, var, sep = " ~ ")
 
   # Loop over adding each spline term to model
@@ -31,6 +32,7 @@ get_model = function(y, smooth_terms, linear_terms, shape_type){
     for (i in 2:p_smooth){
       # Build call to model formula with other spline terms
       var = paste("s(", smooth_terms[i], ", bs = '", shape_type[i], "')", sep = "")
+      # Add new term to model
       scam_model = paste(scam_model, var, sep = " + ")
     }
   }
@@ -39,6 +41,7 @@ get_model = function(y, smooth_terms, linear_terms, shape_type){
   for (i in 1:p_linear){
     # Build call to model formula with linear terms
     var = linear_terms[i]
+    # Add new term to model
     scam_model = paste(scam_model, var, sep = " + ")
   }
   # Return string of call to model
