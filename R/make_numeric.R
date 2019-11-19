b2ae45c8ece4a07494c6b21d68f5c8af1063c144
Christi Thompson <christithomp@tamu.edu>
2019-11-18 20:02:20 -0600

    Create framework for numeric function

---
+++ R/make_numeric.R
@@ -0,0 +1,13 @@
+
+make_numeric = function(data){
+  p = ncol(data) # Get number of variables in data
+
+  for (i in 1:p){
+    if (!is.numeric(data[ , i]){
+      if (is.character(data[ , i]) | is.string(data[ , i])){
+
+      }
+    }
+  }
+
+}
