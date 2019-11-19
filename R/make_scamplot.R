4029cc0b7f26aed01296b900055a4b6e4b14eea9
Christi Thompson <christithomp@tamu.edu>
2019-11-19 11:06:38 -0600

    Add stop() before paste for checks

--- R/make_scamplot.R
+++ R/make_scamplot.R
@@ -2,23 +2,23 @@
 make_scamplot = function(data, y, smooth_terms, linear_terms, shape_type, type, title = "Prediction Plot"){
   # Check if y is a column in the data
   if (y %in% colnames(data)){
-    paste("Error: y argument supplied is not a variable in the data supplied")
+    stop(paste("Error: y argument supplied is not a variable in the data supplied"))
   }
   # Check if smooth_terms are columns in data
   if (any(!(smooth_terms %in% colnames(data)))){
-    paste("Error: some of the smooth_terms supplied are not variables in the data supplied")
+    stop(paste("Error: some of the smooth_terms supplied are not variables in the data supplied"))
   }
   # Check if linear_terms are columns in data
   if (any(!(linear_terms %in% colnames(data)))){
-    paste("Error: some of the linear_terms supplied are not variables in the data supplied")
+    stop(paste("Error: some of the linear_terms supplied are not variables in the data supplied"))
   }
   # Check if type is of the right form
   if (type != "response" | type != "link"){
-    paste("Error: type argument supplied is not a string that contains either link or response")
+    stop(paste("Error: type argument supplied is not a string that contains either link or response"))
   }
   # Check if title is string
   if(!is.string(title)){
-    paste("Error: title argument supplied is not a string")
+    stop(paste("Error: title argument supplied is not a string"))
   }
 
   # Check if shape_type contains string of acceptable values
