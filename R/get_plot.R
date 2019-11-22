60e9e950861fcd559b15dca41ef05607f9a16fbf
Christi Thompson <christithomp@tamu.edu>
2019-11-22 09:48:08 -0600

    Add example to roxygen skeleton and change type to pred_type

--- R/get_plot.R
+++ R/get_plot.R
@@ -8,12 +8,35 @@
 #' @param title - string for title of the plot
 #' @param x_lab - string for title of x axis
 #' @param y_lab - string for title of y axis
-#' @param type - string indicating "link" or "response"
+#' @param pred_type - string indicating "link" or "response"
 #' @return
 #' @export
 #'
 #' @examples
-get_plot = function(x, fHat, lb, ub, title, x_lab, y_lab, type){
+#' # Load required packaged
+#' library(mgcv); library(HRW)
+#' # Load the data
+#' data(BostonMortgages)
+#'
+#' #Create model for data
+#' fit = gam(deny ~ black + s(dir), family = binomial, data = BostonMortgages)
+#'
+#' #Create data frame for prediction
+#' ng = 1001
+#' dir_seq = seq(min(BostonMortgages$dir), max(BostonMortgages$dir), length = ng)
+#' black = BostonMortgages$black
+#' black_seq = rep(unique(black)[which.max(tabulate(match(black, unique(black))))], ng)
+#'
+#' #Get predicted y values, lower bound, and upper bound of confidence interval
+#' newdata = data.frame(dir = dir_seq, black =black_seq)
+#' pred_fit = predict(fit, newdata, se.fit = T)
+#' fHat = pred_fit$fit
+#' lb = fHat - qnorm(0.975) * pred_fit$se.fit
+#' ub = fHat + qnorm(0.975) * pred_fit$se.fit
+#'
+#' #Plot the predictive plot
+#' get_plot(dir_seq, fHat, lb, ub, "Prediction of DIR", x_lab = "dir", y_lab = "deny", pred_type = "response")
+get_plot = function(x, fHat, lb, ub, title, x_lab, y_lab, pred_type){
   # Get number of columns of data
   p = ncol(data)
 
@@ -21,10 +44,10 @@ get_plot = function(x, fHat, lb, ub, title, x_lab, y_lab, type){
   expit <- function(x) return(1/(1 + exp(-x)))
 
   # Creat basic plot template
-  plot(0, type = 'n', xlab = x_lab, ylab = y_lab, xlim = c(0, 1), ylim = c(0, 1),
-       type = "l", lwd = 2, main = title)
+  plot(0, type = 'n', xlab = x_lab, ylab = y_lab, xlim = c(min(x), max(x)), ylim = c(0, 1),
+       lwd = 2, main = title)
   # Check if plotting type = link
-  if (type == "link"){
+  if (pred_type == "link"){
     # Create shaded confidence region
     polygon(c(x, rev(x)), c(lb), rev(ub), col = 'palegreen', border = FALSE)
     # Create line of function shape
