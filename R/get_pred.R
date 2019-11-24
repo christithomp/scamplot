61f25b1d9c81aad7ee87cbf75e91efc2a7ea314f
Christi Thompson <christithomp@tamu.edu>
2019-11-24 14:39:09 -0600

    Add check to make sure new_data is correct form

--- R/get_pred.R
+++ R/get_pred.R
@@ -3,7 +3,6 @@
 #'
 #' @param fit - model used for making predictions
 #' @param new_data - dataframe or matrix used for prediction
-#' @param type - string that gives type of prediction (either "response" or "link")
 #'
 #' @return - list of predictions and lower and upper bounds of confidence interval
 #' @export
@@ -29,6 +28,10 @@
 #' mpg_pred$ub
 #' mpg_pred$lb
 get_pred = function(fit, new_data){
+  # Check if new_data is matrix or dataframe
+  if (!is.data.frame(new_data) & !is.matrix(new_data)){
+    stop(paste("Error: new_data supplied is not a matrix or dataframe"))
+  }
   # Get number of columns of data
   p = ncol(new_data)
   # Get number of rows of data
