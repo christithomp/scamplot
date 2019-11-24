963c2730664d181e18e74a20fa996163773b7122
Christi Thompson <christithomp@tamu.edu>
2019-11-24 14:41:54 -0600

    Chnage type to link in check

--- R/get_plot.R
+++ R/get_plot.R
@@ -39,7 +39,7 @@
 #' get_plot(dir_seq, fHat, lb, ub, "Prediction of DIR", x_lab = "dir", y_lab = "deny", pred_type = "response")
 get_plot = function(x, fHat, lb, ub, title, x_lab, y_lab, pred_type){
   #Check if pred_type is link or response
-  if (pred_type != 'type' & pred_type != 'response'){
+  if (pred_type != 'link' & pred_type != 'response'){
     stop(paste("Error: pred_type supplied is not 'link' or 'response'"))
   }
   # Get number of columns of data
