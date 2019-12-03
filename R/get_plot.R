e97595deffca1386d9b20bca6d97b6f0b5594afb
Christi Thompson <christithomp@tamu.edu>
2019-12-02 19:25:49 -0600

    Make title optional

--- R/get_plot.R
+++ R/get_plot.R
@@ -5,7 +5,7 @@
 #' @param fHat - vector of prediction values
 #' @param lb - vector containing  lower bound of confidence interval
 #' @param ub -vector containing upper bound of confidence interval
-#' @param title - string for title of the plot
+#' @param title - optional string for title of the plot
 #' @param x_lab - string for title of x axis
 #' @param y_lab - string for title of y axis
 #' @param pred_type - string indicating "link" or "response"
@@ -35,9 +35,9 @@
 #' ub = fHat + qnorm(0.975) * pred_fit$se.fit
 #'
 #' #Plot the predictive plot
-#' get_plot(dir_seq, fHat, lb, ub, "Prediction of DIR", x_lab = "dir", y_lab = "deny", pred_type = "link")
-#' get_plot(dir_seq, fHat, lb, ub, "Prediction of DIR", x_lab = "dir", y_lab = "deny", pred_type = "response")
-get_plot = function(x, fHat, lb, ub, title, x_lab, y_lab, pred_type){
+#' get_plot(dir_seq, fHat, lb, ub, x_lab = "dir", y_lab = "deny", pred_type = "link")
+#' get_plot(dir_seq, fHat, lb, ub, x_lab = "dir", y_lab = "deny", pred_type = "response")
+get_plot = function(x, fHat, lb, ub, title = 'Prediction Plot', x_lab, y_lab, pred_type){
   #Check if pred_type is link or response
   if (pred_type != 'link' & pred_type != 'response'){
     stop(paste("Error: pred_type supplied is not 'link' or 'response'"))
